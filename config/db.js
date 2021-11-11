6dad790aebd9cd3eeb79ed6a3050540a2d187c85
Yuri Dias Pereira <yuristardias@hotmail.com>
2021-11-10 21:51:21 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = {mongoURI: "mongodb+srv://yuridiasp:poliana0609@blogapp.0mxqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://usuário:senha@instancia.0mxqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 }
 else {
     module.exports = {mongoURI: "mongodb://localhost/wpp-backup"}
